b6a3c9da2876e86ad10018690e91a08545aacd15
javnson <65205730+javnson@users.noreply.github.com>
2025-08-12 11:34:36 +0800

    update fuzzy controller designer.

--- ctl/component/intrinsic/advance/fuzzy_designer/test_get_fuzzy_matrix.m
+++
@@ -1,5 +0,0 @@
-% Generate a test data for fuzzy controller.
-
-[fuzzy_2d_mat, dim1_mesh, dim2_mesh] = get_fuzzy_matrix('mtr_fuzzy_dsn.fis', 2, [-1.0, 1.0; -1.0, 1.0], [20, 20]);
-
-generate_fuzzy_2d_src('fuzzy_2d', 'fuzzy', fuzzy_2d_mat, dim1_mesh, dim2_mesh);
