7007778e8874e132c38da10eb00e228a11a02940
javnson <65205730+javnson@users.noreply.github.com>
2026-01-28 14:05:05 +0800

    grid current close loop done.

--- ctl/suite/pgs_3ph_GFL_inverter/implement/f280039c_iris_node/xplt.ctl_interface.h
+++ ctl/suite/pgs_3ph_GFL_inverter/implement/f280039c_iris_node/xplt.ctl_interface.h
@@ -101,15 +101,27 @@ GMP_STATIC_INLINE void ctl_output_callback(void)
     DAC_setShadowValue(IRIS_DACB_BASE, iuvw.control_port.value.dat[phase_C] * 2048 + 2048);
 
     // grid voltage and inverter voltage
-    //    DAC_setShadowValue(IRIS_DACB_BASE, uuvw.control_port.value.dat[phase_C] * 2048 + 2048);
-    //    DAC_setShadowValue(IRIS_DACA_BASE, vabc.control_port.value.dat[phase_C] * 2048 + 2048);
-
+    //    DAC_setShadowValue(IRIS_DACB_BASE, uuvw.control_port.value.dat[phase_C] * 2048 + 2048);Zq
     //    DAC_setShadowValue(IRIS_DACB_BASE, EPWM_getCounterCompareValue(PHASE_U_BASE, EPWM_COUNTER_COMPARE_A)/2);
     //    DAC_setShadowValue(IRIS_DACA_BASE, EPWM_getCounterCompareValue(PHASE_V_BASE, EPWM_COUNTER_COMPARE_A)/2);
 
     //    DAC_setShadowValue(IRIS_DACB_BASE, pwm_out.value[2]/2);
     //    DAC_setShadowValue(IRIS_DACA_BASE, pwm_out.value[1]/2);
     //    inv_ctrl.
+
+#elif BUILD_LEVEL == 2
+
+    DAC_setShadowValue(IRIS_DACA_BASE, inv_ctrl.vab0.dat[phase_A] * 2048 + 2048);
+    DAC_setShadowValue(IRIS_DACB_BASE, inv_ctrl.pll.srf_pll.phasor.dat[phasor_cos] * 2048 + 2048);
+
+#elif BUILD_LEVEL == 3
+
+//    DAC_setShadowValue(IRIS_DACA_BASE, inv_ctrl.vabc.dat[phase_A] * 2048 + 2048);
+    DAC_setShadowValue(IRIS_DACA_BASE, inv_ctrl.vab0.dat[phase_A] * 2048 + 2048);
+
+    DAC_setShadowValue(IRIS_DACB_BASE, inv_ctrl.pll.srf_pll.phasor.dat[phasor_cos] * 2048 + 2048);
+//    DAC_setShadowValue(IRIS_DACB_BASE, inv_ctrl.pll.v_pos_seq.dat[0] * 2048 + 2048);
+//    DAC_setShadowValue(IRIS_DACB_BASE, inv_ctrl.pll.srf_pll.theta * 2048 + 2048);
 
 #endif // BUILD_LEVEL
 }
