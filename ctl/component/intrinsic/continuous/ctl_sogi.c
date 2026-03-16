b6c6d587bf6b9b59127ebc4e2132570f12415158
javnson <65205730+javnson@users.noreply.github.com>
2026-03-16 16:53:03 +0800

    upgrade sogi module.

--- ctl/component/intrinsic/continuous/ctl_sogi.c
+++ ctl/component/intrinsic/continuous/ctl_sogi.c
@@ -1,49 +1,5 @@
-//////////////////////////////////////////////////////////////////////////
-// SOGI controller
+#include <gmp_core.h>
 
-#include <ctl/component/intrinsic/continuous/sogi.h>
-
-void ctl_init_sogi_controller(
-    // controller handle
-    ctl_sogi_t* sogi,
-    // gain of this controller
-    parameter_gt gain,
-    // resonant frequency
-    parameter_gt freq_r,
-    // cut frequency
-    parameter_gt freq_c,
-    // controller frequency
-    parameter_gt freq_ctrl)
-{
-    sogi->k_damp = float2ctrl(2 * freq_c / freq_r);
-    sogi->k_r = float2ctrl(CTL_PARAM_CONST_2PI * freq_r / freq_ctrl);
-    sogi->gain = float2ctrl(gain);
-
-    sogi->integrate_reference = 0;
-    sogi->d_integrate = 0;
-    sogi->q_integrate = 0;
-}
-
-void ctl_init_sogi_controller_with_damp(
-    // controller handle
-    ctl_sogi_t* sogi,
-    // gain of this controller
-    parameter_gt gain,
-    // resonant frequency
-    parameter_gt freq_r,
-    // cut frequency, generally 1.414 is a great choice
-    parameter_gt damp,
-    // controller frequency
-    parameter_gt freq_ctrl)
-{
-    sogi->k_damp = float2ctrl(damp);
-    sogi->k_r = float2ctrl(CTL_PARAM_CONST_2PI * freq_r / freq_ctrl);
-    sogi->gain = float2ctrl(gain);
-
-    sogi->integrate_reference = 0;
-    sogi->d_integrate = 0;
-    sogi->q_integrate = 0;
-}
 
 //////////////////////////////////////////////////////////////////////////
 // SOGI controller
@@ -52,6 +8,9 @@ void ctl_init_sogi_controller_with_damp(
 
 void ctl_init_sogi(ctl_sogi_t* sogi, parameter_gt gain, parameter_gt freq_r, parameter_gt damp, parameter_gt fs)
 {
+    // Error prevention engineering: guard against division by zero
+    gmp_base_assert(fs > 0.0f);
+
     // Calculate the sampling period
     parameter_gt Ts = 1.0f / fs;
     // Calculate the resonant angular frequency
