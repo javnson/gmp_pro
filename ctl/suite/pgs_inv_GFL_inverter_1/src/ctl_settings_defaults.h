276ba5c3a21f7991d8abd3d7d38931331c5fc69c
XM-anti <3022903278@qq.com>
2026-07-21 10:33:42 +0800

    7_21_1
    创建一个GFL_inverter复印件

---
+++ ctl/suite/pgs_inv_GFL_inverter_1/src/ctl_settings_defaults.h
@@ -0,0 +1,11 @@
+/**
+ * @file ctl_settings_defaults.h
+ * @brief Compatibility include for the shared GFL inverter SDPE contract.
+ */
+
+#ifndef _FILE_PGS_INV_GFL_CTL_SETTINGS_DEFAULTS_H_
+#define _FILE_PGS_INV_GFL_CTL_SETTINGS_DEFAULTS_H_
+
+#include <sdpe_pgs_inv_gfl_common_settings.h>
+
+#endif // _FILE_PGS_INV_GFL_CTL_SETTINGS_DEFAULTS_H_
